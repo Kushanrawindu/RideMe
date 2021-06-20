17a253809487b775515bfe6ee0bd297ec671926a
Kushanrawindu <kushanrawindu@gmail.com>
2021-06-20 13:30:00 +0530

    splash screen added

--- app/src/main/java/com/example/rideme/SplashScreenActivity.java
+++ app/src/main/java/com/example/rideme/SplashScreenActivity.java
@@ -3,12 +3,27 @@ package com.example.rideme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.functions.Action;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        delaySplashScreen();
+    }
+
+    private void delaySplashScreen() {
+        Completable.timer(5, TimeUnit.SECONDS,
+                AndroidSchedulers.mainThread())
+                .subscribe(() -> Toast.makeText(SplashScreenActivity.this,"Splash Screen done!!!",Toast.LENGTH_SHORT).show());
     }
 }

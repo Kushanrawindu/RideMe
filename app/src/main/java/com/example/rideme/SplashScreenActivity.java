b6bf1d6136116c2ebee7204f2955ee3292405163
Kushanrawindu <kushanrawindu@gmail.com>
2021-06-21 23:54:52 +0530

    set progress bar in splash screen page

--- app/src/main/java/com/example/rideme/SplashScreenActivity.java
+++ app/src/main/java/com/example/rideme/SplashScreenActivity.java
@@ -1,10 +1,13 @@
 package com.example.rideme;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthMethodPickerLayout;
@@ -12,11 +15,18 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.functions.Action;
@@ -27,6 +37,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     private List<AuthUI.IdpConfig> providers;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener listener;
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progress_bar;
+
+    FirebaseDatabase database;
+    DatabaseReference driverInfoRef;
 
     @Override
     protected void onStart() {
@@ -44,11 +60,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash_screen);
         init();
     }
 
     private void init() {
+
+        ButterKnife.bind(this);
+
+        database = FirebaseDatabase.getInstance();
+        driverInfoRef = database.getReference(Common.DRIVER_INFO_REFERENCE);
+
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build());
@@ -57,10 +79,38 @@ public class SplashScreenActivity extends AppCompatActivity {
         listener = myFirebaseAuth -> {
             FirebaseUser user = myFirebaseAuth.getCurrentUser();
             if(user != null)
-                Toast.makeText(this, "Welcome:" +user.getUid(), Toast.LENGTH_SHORT).show();
+            {
+                checkUserFromFirebase();
+            }
             else
                 showLoginLayout();
         };
+    }
+
+    private void checkUserFromFirebase() {
+        driverInfoRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists())
+                        {
+                            Toast.makeText(SplashScreenActivity.this, "User already registered..", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            showRegisterLayout();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(SplashScreenActivity.this, ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void showRegisterLayout() {
+
     }
 
     private void showLoginLayout() {
@@ -80,12 +130,11 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void delaySplashScreen() {
+        progress_bar.setVisibility(View.VISIBLE);
+
         Completable.timer(3, TimeUnit.SECONDS,
                 AndroidSchedulers.mainThread())
-                .subscribe(() ->
-                        firebaseAuth.addAuthStateListener(listener)
-
-                        );
+                .subscribe(() -> firebaseAuth.addAuthStateListener(listener));
     }
 
     @Override
